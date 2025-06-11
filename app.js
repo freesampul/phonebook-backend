a09da7cad5c7c9375267b51049fa9b96dfdd0e7d
Sam <sam.pulaski@LYV.livenation.com>
2025-06-11 10:33:55 -0400

    fixed port

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ app.put('/api/persons/:id', (req, res) => {
 });
 
 // Start server
-const PORT = 3001 ;
+const PORT = process.env.PORT || 3001;
 const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
